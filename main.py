43d4e8af8e10ad48682e809b7720ce84e71af6e8
Demopans <gao.jeff3@gmail.com>
2023-08-25 12:06:13 -0700

    first
    Signed-off-by: Demopans <gao.jeff3@gmail.com>

---
+++ main.py
@@ -0,0 +1,10 @@
+# test script used to ensure model works. When exporting, only export ./model
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    import torch
+
+    # debug
+    print(torch.cuda.is_available())
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ model/model.py
@@ -0,0 +1,5 @@
+# General torch model bindings. infrence.py and fineTune.py contain the bit that actually run infrence and fineTune respectively
+import torch
+
+def initModel():
+    pass
