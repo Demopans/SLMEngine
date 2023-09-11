b433d4c4ed2dd384e68d17749de76305449b2722
Demopans <gao.jeff3@gmail.com>
2023-09-11 11:45:11 -0700

    fill out outline
    Signed-off-by: Demopans <gao.jeff3@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# test script used to ensure model works. When exporting, only export ./model
+# test script used to ensure model works. When exporting, only export ./model into plugin of sorts
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
--- model/model.py
+++ model/model.py
@@ -1,5 +1,14 @@
 # General torch model bindings. infrence.py and fineTune.py contain the bit that actually run infrence and fineTune respectively
 import torch
+import sys
 
-def initModel():
+def initModel(model: torch.nn.Module, config: str):
+    # attach safetensored model to blank model
+
+    pass
+
+def infer():
+    pass
+
+def tune():
     pass
