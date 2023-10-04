9ad6142569cfbefc00528ad7d8a02d7cab853107
Demopans <gao.jeff3@gmail.com>
2023-10-04 19:22:01 -0400

    slight tweak
    Signed-off-by: Demopans <gao.jeff3@gmail.com>

--- model/model.py
+++ model/model.py
@@ -1,19 +1,18 @@
 # General torch model bindings. infrence.py and fineTune.py contain the bit that actually run infrence and fineTune respectively
-
 import torch
 
 
-
 class LM:
-
     model: torch.nn.Module
 
     def __init__(self, config: str):
+        import os.path as osp
         # attach safetensored model to blank model
         # split config file on newlines
+        if not osp.exists(config):
+            raise "No config file. Go get a config file"
 
-
-
+        # debug
         if torch.cuda.is_available():
             dev = "cuda:0"
         else:
@@ -21,10 +20,57 @@ class LM:
 
         from transformers import AutoTokenizer, AutoModelForCausalLM
 
-        self.model = AutoModelForCausalLM.from_pretrained("./model/Wizard-Vicuna-7B-Uncensored-GPTQ", device_map="cuda")
-        self.tokenizer = AutoTokenizer.from_pretrained("./model/Wizard-Vicuna-7B-Uncensored-GPTQ")
+        # note: Triton is currently bugged on Python 3.11
+        modelPath = "./model/Wizard-Vicuna-7B-Uncensored-GPTQ"
+        self.model = AutoModelForCausalLM.from_pretrained(modelPath, device_map="cuda")
+        self.tokenizer = AutoTokenizer.from_pretrained(modelPath)
         self.memory: str
 
+    def infer(self, input="Hello, my name is"):
+        inputs = self.tokenizer(input, return_tensors="pt")
+        inputs = inputs.to(0)
+        output = self.model.generate(inputs["input_ids"])
+        print(output)
+        pass
+
+    def tune(self):
+        pass
+
+
+"""
+# D's Version (Fast Run)
+
+import torch
+import os.path as osp
+from transformers import AutoTokenizer, pipeline, logging
+from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
+class LM:
+
+    model: torch.nn.Module
+
+    def __init__(self, config: str):
+        # attach safetensored model to blank model
+        # split config file on newlines
+        if not osp.exists(config):
+            raise "No config file. Go get a config file"
+
+        # debug
+        if torch.cuda.is_available():
+            dev = "cuda:0"
+        else:
+            dev = "cpu"
+
+        self.tokenizer = AutoTokenizer.from_pretrained("./model/Wizard-Vicuna-7B-Uncensored-GPTQ")
+
+        self.model = AutoGPTQForCausalLM.from_quantized("./model/Wizard-Vicuna-7B-Uncensored-GPTQ",
+                        model_basename="Model",
+                        use_safetensors=True,
+                        trust_remote_code=True,
+                        device="cuda:0",
+                        use_triton=False,
+                        quantize_config=None)
+
+        self.memory: str
 
     def infer(self):
 
@@ -32,3 +78,52 @@ class LM:
 
     def tune(self):
         pass
+"""
+
+# Test Code
+"""
+from transformers import AutoTokenizer, pipeline, logging
+from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
+
+model_name_or_path = "D:\SLMEngine\model\Wizard-Vicuna-7B-Uncensored-GPTQ"
+model_basename = "model"
+
+use_triton = False
+
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+
+model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
+        model_basename=model_basename,
+        use_safetensors=True,
+        trust_remote_code=True,
+        device="cuda:0",
+        use_triton=use_triton,
+        quantize_config=None)
+
+
+prompt = ""
+prompt_template=f'''How are babies made?.
+
+USER: {prompt}
+ASSISTANT:
+'''
+
+# Inference can also be done using transformers' pipeline
+
+# Prevent printing spurious transformers error when using pipeline with AutoGPTQ
+logging.set_verbosity(logging.CRITICAL)
+
+print("*** Pipeline:")
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    do_sample = True,
+    max_new_tokens=512,
+    temperature=0.7,
+    top_p=0.95,
+    repetition_penalty=1.15
+)
+
+print(pipe(prompt_template)[0]['generated_text'])
+"""
